7f1852dcb63724dceceac02ea82ab63511e38cf4
carolliu2024 <CarolLiu2024@u.northwestern.edu>
2023-02-11 14:48:55 -0600

    create loveletter

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
+// Import LoveLetter creator
+import { LoveLetter } from './loveletter'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -27,6 +29,10 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <LoveLetter>
+
+      </LoveLetter>
+
     </div>
   )
 }
---
+++ src/loveletter.jsx
@@ -0,0 +1,76 @@
+import React, { useRef } from 'react';
+import { useState } from 'react';
+//http://www.gingerbeardman.com/loveletter/
+//copied from https://trinket.io/html/c208236a41
+export const LoveLetter = () => {
+
+  const [randomLetter, setRandomLetter] = useState("")  // [value, update_value_function]
+  var letter = "";
+  function lettercreate(){
+    var salutations1 = ["BELOVED", "DARLING", "DEAR", "DEAREST", "FANCIFUL", "HONEY"],
+
+    salutations2 = ["CHICKPEA", "DEAR", "DUCK", "JEWEL", "LOVE", "MOPPET", "SWEETHEART"],
+
+    adverbs = ["AFFECTIONATELY", "ARDENTLY", "ANXIOUSLY", "BEAUTIFULLY",
+        "BURNINGLY", "COVETOUSLY", "CURIOUSLY", "EAGERLY", "FERVENTLY",
+        "FONDLY", "IMPATIENTLY", "KEENLY", "LOVINGLY", "PASSIONATELY",
+        "SEDUCTIVELY", "TENDERLY", "WISTFULLY", ""
+    ],
+
+    adjectives = ["AFFECTIONATE", "AMOROUS", "ANXIOUS", "AVID", "BEAUTIFUL",
+        "BREATHLESS", "BURNING", "COVETOUS", "CRAVING", "CURIOUS",
+        "EAGER", "FERVENT", "FONDEST", "LOVEABLE", "LOVESICK",
+        "LOVING", "PASSIONATE", "PRECIOUS", "SEDUCTIVE", "SWEET",
+        "SYMPATHETIC", "TENDER", "UNSATISFIED", "WINNING", "WISTFUL", ""
+    ],
+
+    nouns = ["ADORATION", "AFFECTION", "AMBITION", "APPETITE", "ARDOUR", "BEING",
+        "BURNING", "CHARM", "CRAVING", "DESIRE", "DEVOTION", "EAGERNESS",
+        "ENCHANTMENT", "ENTHUSIASM", "FANCY", "FELLOW FEELING", "FERVOUR",
+        "FONDNESS", "HEART", "HUNGER", "INFATUATION", "LITTLE LIKING",
+        "LONGING", "LOVE", "LUST", "PASSION", "RAPTURE", "SYMPATHY",
+        "THIRST", "WISH", "YEARNING"
+    ],
+
+    verbs = ["ADORES", "ATTRACTS", "CLINGS TO", "HOLDS DEAR", "HOPES FOR",
+        "HUNGERS FOR", "LIKES", "LONGS FOR", "LOVES", "LUSTS AFTER",
+        "PANTS FOR", "PINES FOR", "SIGHS FOR", "TEMPTS", "THIRSTS FOR",
+        "TREASURES", "YEARNS FOR", "WOOS"
+    ];
+
+    function choose(lst) {
+    var i = Math.floor(Math.random() * lst.length);
+    return lst[i];
+    }
+
+    letter += choose(salutations1) + " " + choose(salutations2) + ",";
+
+    for (var i = 0; i < 5; i = i + 1) {
+        if (Math.random() < 0.5) {
+        letter += "MY " +
+            choose(adjectives) + " " +
+            choose(nouns) + " " + choose(adverbs) + " " +
+            choose(verbs) + " YOUR " +
+            choose(adjectives) + " " + choose(nouns) + ". ";
+        } else {
+        letter += "YOU ARE MY " +
+            choose(adjectives) + " " + choose(nouns) + ". ";
+        }
+    }
+    
+    letter += "YOURS " + choose(adverbs) + ",M.U.C";
+
+    setRandomLetter(letter);
+  }
+
+return(
+    <div>
+        <button onClick={()=>lettercreate()}> Generate Letter </button>
+        <div>{randomLetter}</div>
+    </div>
+    
+)
+
+
+}
+
