9b778081a44844a32db7998474822960ce76cba3
carolliu2024 <CarolLiu2024@u.northwestern.edu>
2023-02-11 13:46:47 -0600

    add blobfish

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAtILIL-7MNKk92j9N4ofjF-3dO1w6IwaM",
+  authDomain: "love-spammer.firebaseapp.com",
+  projectId: "love-spammer",
+  storageBucket: "love-spammer.appspot.com",
+  messagingSenderId: "407393734278",
+  appId: "1:407393734278:web:00e84c88a595b628db4b07"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
